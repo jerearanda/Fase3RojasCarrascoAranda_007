0777fdd3ba75322da647565c1a54e784a50b2718
jachalo <72086399+jachalo@users.noreply.github.com>
2020-12-02 21:21:32 -0300

    Modificado: settings.py
    Se corrige la ruta de la variable STATICFILES_DIRS

--- animespedia/animespedia/settings.py
+++ animespedia/animespedia/settings.py
@@ -126,9 +126,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "..", "static"),)
 
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
